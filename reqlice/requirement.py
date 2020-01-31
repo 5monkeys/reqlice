87ae02504ee30c1ce29ae028dc6e589c82ced1cd
Beshr Kayali <me@beshr.com>
2020-01-31 12:03:54 +0100

    Fix pip api for pip=>10 and read linked requirement files

--- reqlice/requirement.py
+++ reqlice/requirement.py
@@ -1,5 +1,13 @@
-from pip.req import parse_requirements as pip_parse_requirements
-from pip.req import InstallRequirement
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements as pip_parse_requirements
+    from pip._internal.req import InstallRequirement
+    from pip._internal.req.constructors import install_req_from_line as _from_line
+
+except ImportError:
+    from pip.req import parse_requirements as pip_parse_requirements
+    from pip.req import InstallRequirement
+
+    _from_line = InstallRequirement.from_line
 
 
 def is_pypi_requirement(requirement):
@@ -13,19 +21,23 @@ def parse_requirements(path_to_requirements):
     :return: ['package name', ..]
     """
     parsed_reqs = []
-    for requirement in pip_parse_requirements(path_to_requirements,
-                                              session=False):
+    for requirement in pip_parse_requirements(path_to_requirements, session=False):
         if not is_pypi_requirement(requirement):
             continue
 
-        parsed_reqs.append(requirement.req.project_name)
+        try:
+            _name = requirement.req.project_name
+        except AttributeError:
+            _name = requirement.req.name
+
+        parsed_reqs.append(_name)
 
     return parsed_reqs
 
 
 def get_valid_pypi_requirement(line):
     try:
-        requirement = InstallRequirement.from_line(line)
+        requirement = _from_line(line)
         if not is_pypi_requirement(requirement):
             raise ValueError
     except ValueError:
